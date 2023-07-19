7ed017be6a7761dd69315b49bd5258133e58e0f6
xiangmin666 <xm13681152536@163.com>
2023-07-19 01:56:15 -0400

    test

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out("spring.devtools.restart.enabled");
+        System.out("spring.devtools.restart.enabled");
+        System.out("spring.devtools.restart.enabled");
+        System.out("spring.devtools.restart.enabled");
+        System.out("spring.devtools.restart.enabled");
+
+    }
+
+}
